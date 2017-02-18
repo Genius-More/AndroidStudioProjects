dc649d68b79aeac15f08c7a5d2d1765120dbd741
Genius-More <xuhongbo.1010@gmail.com>
2017-02-18 13:14:59 +0800

    modify 2017-02-18

--- MyApplication_3.2/app/src/main/java/com/example/xhb/myapplication_32/MainActivity.java
+++ MyApplication_3.2/app/src/main/java/com/example/xhb/myapplication_32/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.xhb.myapplication_32;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button button;
+    private Button button1;
+    private EditText editText;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button_1);
+        editText = (EditText) findViewById(R.id.edit_text) ;
+        imageView = (ImageView) findViewById(R.id.image_view);
+        button.setOnClickListener(this);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_1:
+                imageView.setImageResource(R.mipmap.xiniu);
+                break;
+            default:
+                break;
+        }
     }
 }
